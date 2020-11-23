9bb9b72516c14da8e497bd370c5cbfeefb3b2add
Lenny Zeitoun <zeitounlenny@gmail.com>
2020-11-23 12:52:00 +0200

    Another fix to install_passive.sh

--- install_passive.sh
+++ install_passive.sh
@@ -60,9 +60,7 @@ installNode() {
 }
 
 exportNVM() {
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  command source ~/.bashrc
 }
 
 install_api() {
